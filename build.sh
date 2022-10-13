7167b02d6c16c6f0d3a41691215a9ddb22f2ad6f
jia.xu <jia.xu@shopee.com>
2022-10-13 13:53:54 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf build && mkdir build && cd build
 #source /opt/rh/devtoolset-11/enable
-export CC=clang
-export CXX=clang++
- ~/cmake-3.23.1-linux-x86_64/bin/cmake .. -DCMAKE_BUILD_TYPE=debug 
+export CC=clang-15
+export CXX=clang++-15
+cmake .. -DCMAKE_BUILD_TYPE=debug 
 nohup ninja 2>&1 > ~/ckbuild.log &
